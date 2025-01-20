4de591ba0f4fb8d6b4e869bd5d5a98abd5af9a7d
Sergio Vazquez <sergivv@gmail.com>
2025-01-20 18:47:59 +0100

    Conexión WiFi y configuración pantalla OLED

--- Control/src/main.cpp
+++ Control/src/main.cpp
@@ -1,18 +1,90 @@
-#include <Arduino.h>
+#include <WiFi.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include "config.h"
 
-// put function declarations here:
-int myFunction(int, int);
+// Configuración SSD1306
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define OLED_ADDRESS 0x3C
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire);
 
-void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+// Variables
+unsigned long lastWiFiCheck = 0;
+const unsigned long wifiCheckInterval = 300000; // Verificar wifi cada 5 min
+
+// Configuración OLED
+void setupOLED()
+{
+  // Inicializa la pantalla OLED
+  if (!display.begin(SSD1306_SWITCHCAPVCC, OLED_ADDRESS))
+  {
+    Serial.println("Error al inicializar OLED");
+    while (true)
+      ;
+  }
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+// Conexión wifi
+void connectWiFi()
+{
+  if (!WiFi.config(local_IP, gateway, subnet))
+  {
+    Serial.println("Fallo en la configuración de IP estática. Usando DHCP...");
+  }
+
+  WiFi.begin(ssid, password);
+  unsigned long startAttemptTime = millis();
+
+  while (WiFi.status() != WL_CONNECTED && millis() - startAttemptTime < 30000) // 30s timeout
+  {
+    delay(500);
+    Serial.print(".");
+  }
+
+  if (WiFi.status() == WL_CONNECTED)
+  {
+    display.clearDisplay();
+    display.println("WiFi conectado!");
+    display.print("IP: ");
+    display.println(WiFi.localIP());
+    display.display();
+  }
+  else
+  {
+    Serial.println("No se pudo conectar a Wi-Fi. Reiniciando...");
+    ESP.restart();
+  }
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+// Comprueba el estado de la conexión WiFi
+void checkWiFiConnection()
+{
+  if (millis() - lastWiFiCheck >= wifiCheckInterval)
+  {
+    lastWiFiCheck = millis();
+    if (WiFi.status() != WL_CONNECTED)
+    {
+      Serial.println("WiFi desconectado. Intentando reconectar...");
+      WiFi.disconnect();
+      WiFi.reconnect();
+    }
+  }
+}
+
+void setup()
+{
+  Serial.begin(115200);
+
+  setupOLED();
+  connectWiFi();
+}
+
+void loop()
+{
+  // Verificar y reconectar Wi-Fi
+  checkWiFiConnection();
 }
