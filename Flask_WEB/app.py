6cfed681c6c791e148e420cd4b9162f9b679b747
Sergio Vazquez <sergivv@gmail.com>
2025-04-16 12:50:40 +0200

    Mostrar menos datos por página y corregir advertencias en código

--- Flask_WEB/app.py
+++ Flask_WEB/app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request
+'''Aplicación Flask para mostrar datos de una base de datos SQLite con paginación'''
 import sqlite3
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -11,7 +12,8 @@ cursor = conn.cursor()
 # Función para obtener los datos con paginación
 
 
-def obtener_datos(pagina=1, por_pagina=25):
+def obtener_datos(pagina=1, por_pagina=20):
+    '''Obtenemos los datos de la base de datos con paginación'''
     offset = (pagina - 1) * por_pagina  # Calculamos el desplazamiento (OFFSET)
     cursor.execute('''
         SELECT t.timestamp, t.temperatura, t.humedad, t.mac, d.dispositivo
@@ -26,6 +28,7 @@ def obtener_datos(pagina=1, por_pagina=25):
 
 
 def contar_total_registros():
+    '''Contamos el total de registros en la tabla temperaturas'''
     cursor.execute('''
         SELECT COUNT(*) FROM temperaturas
     ''')
@@ -35,10 +38,11 @@ def contar_total_registros():
 # Ruta principal para mostrar los datos
 @app.route('/')
 def index():
+    '''Ruta principal para mostrar los datos con paginación'''
     # Parámetros para la paginación
     # Página actual (por defecto es 1)
     pagina = int(request.args.get('pagina', 1))
-    por_pagina = 25  # Número de resultados por página
+    por_pagina = 20  # Número de resultados por página
 
     # Obtener los datos de la base de datos
     datos = obtener_datos(pagina, por_pagina)
